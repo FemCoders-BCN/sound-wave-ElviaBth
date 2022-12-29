900ce56ef3ccff85b6d4c2c2cf44188f336ceb6e
Elvia Benedith <ejbenedith@gmail.com>
2022-12-29 20:18:25 +0100

    animation balls

--- src/components/Join.jsx
+++ src/components/Join.jsx
@@ -7,7 +7,10 @@ function Join() {
     <div className='join main-size'>
       <h1>Join the <span>fun.</span></h1>
       <Formulary/>
-
+      <div className='background-balls0'>
+          <div class="ball11"></div>
+          <div class="ball22"></div>
+      </div>
     </div>
   )
 }
